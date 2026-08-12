12235193a38a012e58112ee9ed68fc35c53c058b
hxragi <mixintrace@gmail.com>
2026-08-12 03:28:43 +0300

    refactor(gpu): move gpu conf to module

--- configuration.nix
+++ configuration.nix
@@ -156,21 +156,6 @@
       enable = true;
       enable32Bit = true;
     };
-    nvidia = {
-      open = true;
-      modesetting.enable = true;
-      powerManagement.enable = true;
-      nvidiaPersistenced = true;
-      prime = {
-        offload = {
-          enable = true;
-          enableOffloadCmd = true;
-        };
-
-        intelBusId = "PCI:0@0:2:0";
-        nvidiaBusId = "PCI:1@0:0:0";
-      };
-    };
   };
 
   nix.settings = {
---
+++ modules/hardware/gpu/nvidia.nix
@@ -0,0 +1,17 @@
+{...}: {
+  nvidia = {
+    open = true;
+    modesetting.enable = true;
+    powerManagement.enable = true;
+    nvidiaPersistenced = true;
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+
+      intelBusId = "PCI:0@0:2:0";
+      nvidiaBusId = "PCI:1@0:0:0";
+    };
+  };
+}
